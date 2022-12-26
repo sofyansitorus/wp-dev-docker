bba541465e2e5063481f34fa976c7af73e71cdb7
Sofyan Sitorus <sofyansitorus@gmail.com>
2022-12-26 19:04:13 +0100

    Fix incorrect DB name in config and add option to set WP constants

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const readline = require('readline')
+const fs = require('fs');
+const readline = require('readline');
 
 const generateDockerFile = ({
     containerUser,
@@ -12,46 +12,46 @@ const generateDockerFile = ({
 }) => {
     fs.readFile('templates/Dockerfile', 'utf8', (err, template) => {
         if (err) {
-            console.error(err)
-            return
+            console.error(err);
+            return;
         }
 
         const output = template
             .split('\n')
-            .filter(line => {
+            .filter((line) => {
                 if (-1 !== line.indexOf('ssh-keygen')) {
-                    return 'yes' === generateSSHKey
+                    return 'yes' === generateSSHKey;
                 }
 
                 if (-1 !== line.indexOf('{{gitUserName}}')) {
-                    return !!gitUserName
+                    return !!gitUserName;
                 }
 
                 if (-1 !== line.indexOf('{{gitUserEmail}}')) {
-                    return !!gitUserEmail
+                    return !!gitUserEmail;
                 }
 
                 if (-1 !== line.indexOf('WORKDIR')) {
-                    return !!workDir
+                    return !!workDir;
                 }
 
-                return true
+                return true;
             })
-            .map(line => {
+            .map((line) => {
                 return line
                     .replace(new RegExp('{{image}}', 'g'), image)
                     .replace(new RegExp('{{containerUser}}', 'g'), containerUser)
                     .replace(new RegExp('{{gitUserName}}', 'g'), gitUserName)
                     .replace(new RegExp('{{gitUserEmail}}', 'g'), gitUserEmail)
-                    .replace(new RegExp('{{workDir}}', 'g'), workDir)
+                    .replace(new RegExp('{{workDir}}', 'g'), workDir);
             })
-            .join('\n')
+            .join('\n');
 
-        fs.writeFileSync(`${outputLocation}/Dockerfile`, output)
+        fs.writeFileSync(`${outputLocation}/Dockerfile`, output);
 
-        console.log(`Successfully created Dockerfile file at ${outputLocation}`)
-    })
-}
+        console.log(`Successfully created Dockerfile file at ${outputLocation}`);
+    });
+};
 
 const generateDockerCompose = ({
     containerId,
@@ -61,130 +61,156 @@ const generateDockerCompose = ({
     outputLocation,
     shareSSHKey,
     volumes,
+    wpDebug,
+    wpDebugLog,
+    wpDebugDisplay,
+    scriptDebug,
 }) => {
     fs.readFile('templates/docker-compose.yml', 'utf8', (err, template) => {
         if (err) {
-            console.error(err)
-            return
+            console.error(err);
+            return;
         }
 
         const output = template
             .split('\n')
-            .filter(line => {
+            .filter((line) => {
                 if (-1 !== line.indexOf('~/.ssh')) {
-                    return 'yes' === shareSSHKey
+                    return 'yes' === shareSSHKey;
                 }
 
                 if (-1 !== line.indexOf('name: {{network}}')) {
-                    return !!network
+                    return !!network;
                 }
 
-                return true
+                return true;
             })
-            .map(line => {
+            .map((line) => {
                 return line
                     .replace(new RegExp('{{containerId}}', 'g'), containerId)
                     .replace(new RegExp('{{containerUser}}', 'g'), containerUser)
                     .replace(new RegExp('{{network}}', 'g'), network)
+                    .replace(new RegExp('{{wpDebug}}', 'g'), wpDebug)
+                    .replace(new RegExp('{{wpDebugLog}}', 'g'), wpDebugLog)
+                    .replace(new RegExp('{{wpDebugDisplay}}', 'g'), wpDebugDisplay)
+                    .replace(new RegExp('{{scriptDebug}}', 'g'), scriptDebug);
             })
             .reduce((accumulator, currentValue) => {
-                if (environments && -1 !== currentValue.indexOf('WORDPRESS_DB_NAME=WORDPRESS_DB_NAME')) {
-                    return accumulator.concat(currentValue, [...environments.split(';').map(environment => `      - ${environment}`)])
+                if (
+                    environments &&
+                    -1 !== currentValue.indexOf('WORDPRESS_DB_NAME=wordpress')
+                ) {
+                    return accumulator.concat(currentValue, [
+                        ...environments
+                            .split(';')
+                            .map((environment) => `      - ${environment}`),
+                    ]);
                 }
 
-                if (volumes && -1 !== currentValue.indexOf('var_www_html:/var/www/html')) {
-                    return accumulator.concat(currentValue, [...volumes.split(';').map(volume => `      - ${volume}`)])
+                if (
+                    volumes &&
+                    -1 !== currentValue.indexOf('var_www_html:/var/www/html')
+                ) {
+                    return accumulator.concat(currentValue, [
+                        ...volumes.split(';').map((volume) => `      - ${volume}`),
+                    ]);
                 }
 
-                return accumulator.concat(currentValue)
+                return accumulator.concat(currentValue);
             }, [])
-            .join('\n')
+            .join('\n');
 
-        fs.writeFileSync(`${outputLocation}/docker-compose.yml`, output)
+        fs.writeFileSync(`${outputLocation}/docker-compose.yml`, output);
 
-        console.log(`Successfully created docker-compose.yml file at ${outputLocation}`)
-    })
-}
+        console.log(
+            `Successfully created docker-compose.yml file at ${outputLocation}`
+        );
+    });
+};
 
 const validateEmail = (email) => {
     return String(email)
         .toLowerCase()
         .match(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        )
-}
+        );
+};
 
 const askQuestion = (rl, question) => {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         rl.question(question, (answer) => {
-            resolve(answer)
-        })
-    })
-}
+            resolve(answer);
+        });
+    });
+};
 
 const askQuestions = (questions) => {
     return new Promise(async (resolve, reject) => {
         const rl = readline.createInterface({
             input: process.stdin,
-            output: process.stdout
-        })
+            output: process.stdout,
+        });
 
-        const answers = []
+        const answers = [];
 
         for (let i = 0; i < questions.length; i++) {
-            const { id, text, defaultAnswer, isRequired, isSkip } = questions[i]
+            const { id, text, defaultAnswer, isRequired, isSkip } = questions[i];
 
             if (isSkip && isSkip.call(null, answers)) {
-                continue
+                continue;
             }
 
-            let question = isRequired ? `(*) ${text}` : text
+            let question = isRequired ? `(*) ${text}` : text;
 
             if (defaultAnswer) {
-                question += ` (${defaultAnswer})`
+                question += ` (${defaultAnswer})`;
             }
 
-            let answer = await askQuestion(rl, question + ' ')
+            let answer = await askQuestion(rl, question + ' ');
 
             if ('' === answer && defaultAnswer) {
-                answer = defaultAnswer
+                answer = defaultAnswer;
             }
 
             answers.push({
                 id,
                 answer,
-            })
+            });
         }
 
-        rl.close()
+        rl.close();
 
-        let errorMessage
+        let errorMessage;
 
         questions.forEach(({ id, isSkip, isRequired, validation }) => {
             if (isSkip && isSkip.call(null, answers)) {
-                return
+                return;
             }
 
-            const answer = answers.find(answer => answer.id === id)?.answer ?? ''
+            const answer = answers.find((answer) => answer.id === id)?.answer ?? '';
 
-            if ('' !== answer && validation && !validation.call(null, answer, answers)) {
-                errorMessage = `ERROR: The ${id} input is invalid.`
-                return false
+            if (
+                '' !== answer &&
+                validation &&
+                !validation.call(null, answer, answers)
+            ) {
+                errorMessage = `ERROR: The ${id} input is invalid.`;
+                return false;
             }
 
             if ('' === answer && isRequired) {
-                errorMessage = `ERROR: The ${id} input cannot be left blank. Please enter a value.`
-                return false
+                errorMessage = `ERROR: The ${id} input cannot be left blank. Please enter a value.`;
+                return false;
             }
-        })
+        });
 
         if (errorMessage) {
-            reject(errorMessage)
+            reject(errorMessage);
         }
 
-        resolve(answers)
-    })
-}
+        resolve(answers);
+    });
+};
 
 askQuestions([
     {
@@ -209,8 +235,8 @@ askQuestions([
         text: 'Do you want to generate an SSH key in the container?',
         defaultAnswer: 'yes',
         isSkip: (answers) => {
-            return 'yes' === answers.find(({ id }) => 'shareSSHKey' === id)?.answer
-        }
+            return 'yes' === answers.find(({ id }) => 'shareSSHKey' === id)?.answer;
+        },
     },
     {
         id: 'workDir',
@@ -243,27 +269,69 @@ askQuestions([
         validation: validateEmail,
     },
     {
+        id: 'wpDebug',
+        text: 'Do you want to set WP_DEBUG constant to true?',
+        defaultAnswer: 'yes',
+    },
+    {
+        id: 'wpDebugLog',
+        text: 'Do you want to set WP_DEBUG_LOG constant to true?',
+        defaultAnswer: 'yes',
+    },
+    {
+        id: 'wpDebugDisplay',
+        text: 'Do you want to set WP_DEBUG_DISPLAY constant to true?',
+        defaultAnswer: 'yes',
+    },
+    {
+        id: 'scriptDebug',
+        text: 'Do you want to set SCRIPT_DEBUG constant to true?',
+        defaultAnswer: 'yes',
+    },
+    {
         id: 'outputLocation',
         text: 'Please enter the location on the host where the output should be saved:',
         isRequired: true,
     },
 ])
-    .then(answers => {
-        const image = answers.find(({ id }) => 'image' === id)?.answer
-        const containerUser = answers.find(({ id }) => 'containerUser' === id)?.answer
-        const generateSSHKey = answers.find(({ id }) => 'generateSSHKey' === id)?.answer
-        const gitUserName = answers.find(({ id }) => 'gitUserName' === id)?.answer
-        const gitUserEmail = answers.find(({ id }) => 'gitUserEmail' === id)?.answer
-        const workDir = answers.find(({ id }) => 'workDir' === id)?.answer
-        const containerId = answers.find(({ id }) => 'containerId' === id)?.answer
-        const environments = answers.find(({ id }) => 'environments' === id)?.answer
-        const volumes = answers.find(({ id }) => 'volumes' === id)?.answer
-        const network = answers.find(({ id }) => 'network' === id)?.answer
-        const shareSSHKey = answers.find(({ id }) => 'shareSSHKey' === id)?.answer
-        const outputLocation = answers.find(({ id }) => 'outputLocation' === id)?.answer
+    .then((answers) => {
+        const image = answers.find(({ id }) => 'image' === id)?.answer;
+        const containerUser = answers.find(
+            ({ id }) => 'containerUser' === id
+        )?.answer;
+        const generateSSHKey = answers.find(
+            ({ id }) => 'generateSSHKey' === id
+        )?.answer;
+        const gitUserName = answers.find(({ id }) => 'gitUserName' === id)?.answer;
+        const gitUserEmail = answers.find(({ id }) => 'gitUserEmail' === id)?.answer;
+        const workDir = answers.find(({ id }) => 'workDir' === id)?.answer;
+        const containerId = answers.find(({ id }) => 'containerId' === id)?.answer;
+        const environments = answers.find(({ id }) => 'environments' === id)?.answer;
+        const volumes = answers.find(({ id }) => 'volumes' === id)?.answer;
+        const network = answers.find(({ id }) => 'network' === id)?.answer;
+        const shareSSHKey = answers.find(({ id }) => 'shareSSHKey' === id)?.answer;
+        const wpDebug =
+            'yes' === answers.find(({ id }) => 'wpDebug' === id)?.answer
+                ? 'true'
+                : 'false';
+        const wpDebugLog =
+            'yes' === answers.find(({ id }) => 'wpDebugLog' === id)?.answer
+                ? 'true'
+                : 'false';
+        const wpDebugDisplay =
+            'yes' === answers.find(({ id }) => 'wpDebugDisplay' === id)?.answer
+                ? 'true'
+                : 'false';
+        const scriptDebug =
+            'yes' === answers.find(({ id }) => 'scriptDebug' === id)?.answer
+                ? 'true'
+                : 'false';
+        const outputLocation = answers.find(
+            ({ id }) => 'outputLocation' === id
+        )?.answer;
 
         if (!fs.existsSync(outputLocation)) {
-            fs.mkdirSync(outputLocation, { recursive: true })
+            fs.mkdirSync(outputLocation, { recursive: true });
         }
 
         generateDockerFile({
@@ -274,7 +342,7 @@ askQuestions([
             image,
             outputLocation,
             workDir,
-        })
+        });
 
         generateDockerCompose({
             containerId,
@@ -284,8 +352,12 @@ askQuestions([
             outputLocation,
             shareSSHKey,
             volumes,
-        })
+            wpDebug,
+            wpDebugLog,
+            wpDebugDisplay,
+            scriptDebug,
+        });
     })
     .catch((error) => {
-        console.error(error)
-    })
+        console.error(error);
+    });
