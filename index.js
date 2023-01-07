a27a951dd0da86ae8659e16023c12521eac9c6cc
Sofyan Sitorus <sofyansitorus@gmail.com>
2023-01-07 20:27:45 +0000

    Added input to set NodeJS version

--- index.js
+++ index.js
@@ -1,16 +1,166 @@
 const fs = require('fs');
 const readline = require('readline');
 
+const wpVersions = [
+    {
+        wp: '5.0',
+        php: {
+            min: '5.6',
+            max: '7.3',
+        }
+    },
+    {
+        wp: '5.1',
+        php: {
+            min: '5.6',
+            max: '7.3',
+        }
+    },
+    {
+        wp: '5.2',
+        php: {
+            min: '5.6',
+            max: '7.3',
+        }
+    },
+    {
+        wp: '5.3',
+        php: {
+            min: '5.6',
+            max: '7.4',
+        }
+    },
+    {
+        wp: '5.4',
+        php: {
+            min: '5.6',
+            max: '7.4',
+        }
+    },
+    {
+        wp: '5.5',
+        php: {
+            min: '5.6',
+            max: '7.4',
+        }
+    },
+    {
+        wp: '5.6',
+        php: {
+            min: '5.6',
+            max: '8.0',
+        }
+    },
+    {
+        wp: '5.7',
+        php: {
+            min: '5.6',
+            max: '8.0',
+        }
+    },
+    {
+        wp: '5.8',
+        php: {
+            min: '5.6',
+            max: '8.0',
+        }
+    },
+    {
+        wp: '5.9',
+        php: {
+            min: '5.6',
+            max: '8.1',
+        }
+    },
+    {
+        wp: '6.0',
+        php: {
+            min: '5.6',
+            max: '8.1',
+        }
+    },
+    {
+        wp: '6.1',
+        php: {
+            min: '5.6',
+            max: '8.2',
+        }
+    }
+];
+
 const phpVersions = [
-    '5.6',
-    '7.0',
-    '7.1',
-    '7.2',
-    '7.3',
-    '7.4',
-    '8.0',
-    '8.1',
-    '8.2',
+    {
+        php: '5.6',
+        os: 'Debian GNU/Linux 9 (stretch)',
+        node: {
+            min: '10',
+            max: '17'
+        }
+    },
+    {
+        php: '7.0',
+        os: 'Debian GNU/Linux 9 (stretch)',
+        node: {
+            min: '10',
+            max: '17'
+        }
+    },
+    {
+        php: '7.1',
+        os: 'Debian GNU/Linux 10 (buster)',
+        node: {
+            min: '10',
+            max: '19'
+        }
+    },
+    {
+        php: '7.2',
+        os: 'Debian GNU/Linux 10 (buster)',
+        node: {
+            min: '10',
+            max: '19'
+        }
+    },
+    {
+        php: '7.3',
+        os: 'Debian GNU/Linux 11 (bullseye)',
+        node: {
+            min: '10',
+            max: '19'
+        }
+    },
+    {
+        php: '7.4',
+        os: 'Debian GNU/Linux 11 (bullseye)',
+        node: {
+            min: '10',
+            max: '19'
+        }
+    },
+    {
+        php: '8.0',
+        os: 'Debian GNU/Linux 11 (bullseye)',
+        node: {
+            min: '10',
+            max: '19'
+        }
+    },
+    {
+        php: '8.1',
+        os: 'Debian GNU/Linux 11 (bullseye)',
+        node: {
+            min: '10',
+            max: '19'
+        }
+    },
+    {
+        php: '8.2',
+        os: 'Debian GNU/Linux 11 (bullseye)',
+        node: {
+            min: '10',
+            max: '19'
+        }
+    }
 ];
 
 const compareSemver = (aVersion, bVersion) => {
@@ -98,6 +248,7 @@ const generateDockerFile = ({
     outputLocation,
     phpVersion,
     wpVersion,
+    nodeVersion,
 }) => {
     fs.readFile(`templates/php${phpVersion}/Dockerfile`, 'utf8', (err, template) => {
         if (err) {
@@ -105,7 +256,7 @@ const generateDockerFile = ({
             return;
         }
 
-        const wpVersionNormalized = 'latest' === wpVersion ? wpVersion : `wordpress-${wpVersion}`
+        const wpVersionNormalized = 'latest' === wpVersion ? wpVersion : `wordpress-${wpVersion}`;
 
         const output = template
             .split('\n')
@@ -127,6 +278,7 @@ const generateDockerFile = ({
             .map((line) => {
                 return line
                     .replace(new RegExp('{wpVersion}', 'g'), wpVersionNormalized)
+                    .replace(new RegExp('{nodeVersion}', 'g'), nodeVersion)
                     .replace(new RegExp('{gitUserName}', 'g'), gitUserName)
                     .replace(new RegExp('{gitUserEmail}', 'g'), gitUserEmail)
             })
@@ -309,14 +461,40 @@ const generateHtaccess = ({
     });
 };
 
-const askQuestion = (rl, question, defaultAnswer) => {
+const getErrorMessage = (question, answer, answers) => {
+    const { id, isRequired, validation } = question;
+
+    if ('' === answer && isRequired) {
+        return `ERROR: The ${id} input cannot be left blank. Please enter a value.`;
+    }
+
+    if ('' !== answer && validation) {
+        const isValid = validation.call(null, answer, answers);
+
+        if (false === isValid) {
+            return `ERROR: The ${id} input is invalid.`;
+        }
+
+        if ('string' === typeof isValid) {
+            return `ERROR: ${isValid}`;
+        }
+    }
+
+    return null;
+}
+
+const askQuestion = (rl, question, defaultAnswer, answers) => {
     return new Promise((resolve) => {
         rl.question(question, (answer) => {
             resolve(answer);
         });
 
         if (defaultAnswer) {
-            rl.write(defaultAnswer);
+            if ('function' === typeof defaultAnswer) {
+                rl.write(defaultAnswer(answers));
+            } else {
+                rl.write(defaultAnswer);
+            }
         }
     });
 };
@@ -328,6 +506,7 @@ const askQuestions = (questions) => {
             output: process.stdout,
         });
 
+        let errorMessage;
         const answers = [];
 
         for (let i = 0; i < questions.length; i++) {
@@ -340,7 +519,7 @@ const askQuestions = (questions) => {
             let repeatCounter = 1;
             let question = isRequired ? `(*) ${text}` : `(?) ${text}`
 
-            let answer = await askQuestion(rl, isRepeat ? `${question} (#${repeatCounter}) ` : `${question} `, defaultAnswer)
+            let answer = await askQuestion(rl, isRepeat ? `${question} (#${repeatCounter}) ` : `${question} `, defaultAnswer, answers)
 
             if (isRepeat) {
                 const answersRepeat = [];
@@ -348,51 +527,25 @@ const askQuestions = (questions) => {
                 while ('' !== answer) {
                     answersRepeat.push(answer)
 
-                    answer = await askQuestion(rl, `${question} (#${++repeatCounter}) `, defaultAnswer)
+                    answer = await askQuestion(rl, `${question} (#${++repeatCounter}) `, defaultAnswer, answers)
                 }
 
-                answers.push({
-                    id,
-                    answer: answersRepeat.filter((value, index, self) => self.indexOf(value) === index),
-                });
-            } else {
-                answers.push({
-                    id,
-                    answer,
-                });
+                answer = answersRepeat.filter((value, index, self) => self.indexOf(value) === index);
             }
+
+            errorMessage = getErrorMessage(questions[i], answer, answers);
+
+            if (errorMessage) {
+                break;
+            }
+
+            answers.push({
+                id,
+                answer,
+            });
         }
 
         rl.close();
-
-        let errorMessage;
-
-        questions.forEach(({ id, isSkip, isRequired, validation }) => {
-            if (isSkip && isSkip.call(null, answers)) {
-                return;
-            }
-
-            const answer = answers.find((answer) => answer.id === id)?.answer ?? '';
-
-            if ('' !== answer && validation) {
-                const isValid = validation.call(null, answer, answers);
-
-                if (false === isValid) {
-                    errorMessage = `ERROR: The ${id} input is invalid.`;
-                    return false;
-                }
-
-                if ('string' === typeof isValid || isValid instanceof String) {
-                    errorMessage = `ERROR: ${isValid}`;
-                    return false;
-                }
-            }
-
-            if ('' === answer && isRequired) {
-                errorMessage = `ERROR: The ${id} input cannot be left blank. Please enter a value.`;
-                return false;
-            }
-        });
 
         if (errorMessage) {
             reject(errorMessage);
@@ -404,23 +557,6 @@ const askQuestions = (questions) => {
 
 askQuestions([
     {
-        id: 'phpVersion',
-        text: 'Please enter the PHP version:',
-        defaultAnswer: '7.4',
-        isRequired: true,
-        validation: (phpVersion) => {
-            if (!validateSemver(phpVersion)) {
-                return false;
-            }
-
-            if (-1 === phpVersions.indexOf(phpVersion)) {
-                return `The allowed PHP versions are ${phpVersions.join(', ')}.`;
-            }
-
-            return true;
-        }
-    },
-    {
         id: 'wpVersion',
         text: 'Please enter the WordPress version:',
         defaultAnswer: 'latest',
@@ -430,15 +566,159 @@ askQuestions([
                 return true;
             }
 
-            const wpVersionMin = '5.0';
-            const wpVersionMax = '6.1.1';
-            const isValid = validateSemver(wpVersion);
-
-            if (isValid && (- 1 === compareSemver(wpVersion, wpVersionMin) || 1 === compareSemver(wpVersion, wpVersionMax))) {
-                return `The allowed WordPress version is from ${wpVersionMin} up to ${wpVersionMax}.`;
+            if (!validateSemver(wpVersion)) {
+                return false;
             }
 
-            return isValid;
+            const wpVersionParts = wpVersion.split('.');
+            const wpVersionMajor = wpVersionParts?.[0] ?? 0;
+            const wpVersionMinor = wpVersionParts?.[1] ?? 0;
+
+            if (!wpVersions.find(v => v.wp === `${wpVersionMajor}.${wpVersionMinor}`)) {
+                const wpVersionMin = wpVersions[0].wp;
+                const wpVersionMax = wpVersions[(wpVersions.length - 1)].wp;
+
+                return `The allowed range of WordPress versions is from ${wpVersionMin} to ${wpVersionMax}.`;
+            }
+
+            return true;
+        }
+    },
+    {
+        id: 'phpVersion',
+        text: 'Please enter the PHP version:',
+        defaultAnswer: (answers) => {
+            let wpVersion = answers.find(answer => 'wpVersion' === answer.id)?.answer ?? '';
+
+            if ('latest' === wpVersion) {
+                wpVersion = wpVersions[(wpVersions.length - 1)].wp;
+            }
+
+            if (!validateSemver(wpVersion)) {
+                return '';
+            }
+
+            const wpVersionParts = wpVersion.split('.');
+            const wpVersionMajor = wpVersionParts?.[0] ?? 0;
+            const wpVersionMinor = wpVersionParts?.[1] ?? 0;
+
+            return wpVersions.find(v => v.wp === `${wpVersionMajor}.${wpVersionMinor}`)?.php?.max ?? '';
+        },
+        isRequired: true,
+        validation: (phpVersion, answers) => {
+            if (!validateSemver(phpVersion)) {
+                return false;
+            }
+
+            if (!phpVersions.find(v => v.php === phpVersion)) {
+                return `The allowed PHP versions are ${phpVersions.map((v) => v.php).join(', ')}.`;
+            }
+
+            let wpVersion = answers.find(answer => 'wpVersion' === answer.id)?.answer ?? '';
+
+            if ('latest' === wpVersion) {
+                wpVersion = wpVersions[(wpVersions.length - 1)].wp;
+            }
+
+            const wpVersionParts = wpVersion.split('.');
+            const wpVersionMajor = wpVersionParts?.[0] ?? 0;
+            const wpVersionMinor = wpVersionParts?.[1] ?? 0;
+            const phpVersionMin = wpVersions.find(v => v.wp === `${wpVersionMajor}.${wpVersionMinor}`)?.php?.min ?? '0.0';
+            const phpVersionMax = wpVersions.find(v => v.wp === `${wpVersionMajor}.${wpVersionMinor}`)?.php?.max ?? '0.0';
+
+            if (- 1 === compareSemver(phpVersion, phpVersionMin) || 1 === compareSemver(phpVersion, phpVersionMax)) {
+                return `PHP version ${wpVersion} is not complatible with WordPress version ${wpVersion}. The allowed PHP versions are ${phpVersionMin} to ${phpVersionMax}.`;
+            }
+
+            return true;
+        }
+    },
+    {
+        id: 'nodeVersion',
+        text: 'Please enter the NodeJS version:',
+        isRequired: true,
+        defaultAnswer: (answers) => {
+            const phpVersion = answers.find(answer => 'phpVersion' === answer.id)?.answer ?? '';
+
+            const phpVersionParts = phpVersion.split('.');
+            const phpVersionMajor = phpVersionParts?.[0] ?? 0;
+            const phpVersionMinor = phpVersionParts?.[1] ?? 0;
+
+            return phpVersions.find(v => v.php === `${phpVersionMajor}.${phpVersionMinor}`)?.node?.max ?? '19';
+        },
+        validation: (nodeVersion, answers) => {
+            const phpVersion = answers.find(answer => 'phpVersion' === answer.id)?.answer ?? '';
+            const phpVersionParts = phpVersion.split('.');
+            const phpVersionMajor = phpVersionParts?.[0] ?? 0;
+            const phpVersionMinor = phpVersionParts?.[1] ?? 0;
+            const nodeVersionMin = phpVersions.find(v => v.php === `${phpVersionMajor}.${phpVersionMinor}`)?.node?.min ?? '0.0';
+            const nodeVersionMax = phpVersions.find(v => v.php === `${phpVersionMajor}.${phpVersionMinor}`)?.node?.max ?? '0.0';
+
+            if (- 1 === compareSemver(nodeVersion, nodeVersionMin) || 1 === compareSemver(nodeVersion, nodeVersionMax)) {
+                return `NodeJS version ${nodeVersion} is not complatible with PHP version ${phpVersion} image. The allowed NodeJS versions are ${nodeVersionMin} to ${nodeVersionMax}.`;
+            }
+
+            return true;
+        }
+    },
+    {
+        id: 'containerId',
+        text: 'Please enter the ID of the container:',
+        defaultAnswer: 'wpdev',
+        isRequired: true,
+        validation: (containerId) => {
+            if (!/^[a-zA-Z0-9_]+$/.test(containerId)) {
+                return 'The container ID can only contain letters, numbers, and underscores.';
+            }
+
+            return true;
+        }
+    },
+    {
+        id: 'volumes',
+        text: 'Please enter a volume parameter (Example: /path/in/host:/path/in/container):',
+        isRepeat: true,
+        validation: (volumes) => {
+            if (volumes.find(volume => 2 > volume.split(':').length)) {
+                return false;
+            }
+
+            return true;
+        }
+    },
+    {
+        id: 'environments',
+        text: 'Please enter an environment parameter (Example: WORDPRESS_DEBUG=true):',
+        isRepeat: true,
+        validation: (environments) => {
+            if (environments.find(environment => 2 > environment.split('=').length)) {
+                return false;
+            }
+
+            return true;
+        }
+    },
+    {
+        id: 'constants',
+        text: 'Please enter a WordPress constant (Example: SCRIPT_DEBUG=true):',
+        isRepeat: true,
+        validation: (constants) => {
+            if (constants.find(constant => 2 > constant.split('=').length)) {
+                return false;
+            }
+
+            return true;
+        }
+    },
+    {
+        id: 'network',
+        text: 'Please enter the name of the network for the container:',
+        validation: (containerId) => {
+            if (!/^[a-zA-Z_]+$/.test(containerId)) {
+                return 'The network can only contain letters and underscores.';
+            }
+
+            return true;
         }
     },
     {
@@ -455,37 +735,12 @@ askQuestions([
         },
     },
     {
-        id: 'containerId',
-        text: 'Please enter the ID of the container:',
-        defaultAnswer: 'wpdev',
-        isRequired: true,
-    },
-    {
-        id: 'environments',
-        text: 'Please enter an environment parameter (Example: WORDPRESS_DEBUG=true):',
-        isRepeat: true,
-    },
-    {
-        id: 'volumes',
-        text: 'Please enter a volume parameter (Example: /path/in/host:/path/in/container):',
-        isRepeat: true,
-    },
-    {
-        id: 'constants',
-        text: 'Please enter a WordPress constant (Example: SCRIPT_DEBUG=true):',
-        isRepeat: true,
-    },
-    {
-        id: 'network',
-        text: 'Please enter the name of the network for the container:',
-    },
-    {
         id: 'gitUserName',
-        text: 'Please enter your Git user name:',
+        text: 'Please enter your Git config user.name:',
     },
     {
         id: 'gitUserEmail',
-        text: 'Please enter your Git user email:',
+        text: 'Please enter your Git config user.email:',
         validation: validateEmail,
     },
     {
@@ -496,18 +751,19 @@ askQuestions([
     },
 ])
     .then((answers) => {
-        const phpVersion = answers.find(({ id }) => 'phpVersion' === id)?.answer;
         const wpVersion = answers.find(({ id }) => 'wpVersion' === id)?.answer;
-        const constants = answers.find(({ id }) => 'constants' === id)?.answer;
+        const phpVersion = answers.find(({ id }) => 'phpVersion' === id)?.answer;
         const containerId = answers.find(({ id }) => 'containerId' === id)?.answer;
+        const volumes = answers.find(({ id }) => 'volumes' === id)?.answer;
         const environments = answers.find(({ id }) => 'environments' === id)?.answer;
+        const constants = answers.find(({ id }) => 'constants' === id)?.answer;
+        const network = answers.find(({ id }) => 'network' === id)?.answer;
+        const shareSSHKey = answers.find(({ id }) => 'shareSSHKey' === id)?.answer;
         const generateSSHKey = answers.find(({ id }) => 'generateSSHKey' === id)?.answer;
         const gitUserEmail = answers.find(({ id }) => 'gitUserEmail' === id)?.answer;
         const gitUserName = answers.find(({ id }) => 'gitUserName' === id)?.answer;
-        const network = answers.find(({ id }) => 'network' === id)?.answer;
+        const nodeVersion = answers.find(({ id }) => 'nodeVersion' === id)?.answer;
         const outputLocation = answers.find(({ id }) => 'outputLocation' === id)?.answer;
-        const shareSSHKey = answers.find(({ id }) => 'shareSSHKey' === id)?.answer;
-        const volumes = answers.find(({ id }) => 'volumes' === id)?.answer;
 
         if (!fs.existsSync(outputLocation)) {
             fs.mkdirSync(outputLocation, { recursive: true });
@@ -520,6 +776,7 @@ askQuestions([
             outputLocation,
             phpVersion,
             wpVersion,
+            nodeVersion,
         });
 
         generateDockerCompose({
